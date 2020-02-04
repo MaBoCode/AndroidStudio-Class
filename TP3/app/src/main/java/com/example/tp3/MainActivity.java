c3ba13a2b48cb288e4eef6ba52ee9beb4ba184b3
Matthias <matthias.brown@etu.univ-tours.fr>
2020-02-04 18:12:04 +0100

    New Project TP3

--- TP3/app/src/main/java/com/example/tp3/MainActivity.java
+++ TP3/app/src/main/java/com/example/tp3/MainActivity.java
@@ -1,23 +1,14 @@
-package com.example.calculator;
+package com.example.tp3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button nbr_0, nbr_1, nbr_2, nbr_3, nbr_4, nbr_5, nbr_6, nbr_7, nbr_8, nbr_9;
-    private Button op_add, op_sous, op_mult, op_div;
-    private Button back, cancel, equals;
-
-    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-
 }
